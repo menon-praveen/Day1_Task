1dd7e4f3a5aaf3667c397255dcfb55e23f458419
praveenmenon <praveenmenon2005@gmail.com>
2026-06-08 16:56:53 +0530

    updating db url

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,17 +1,14 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
-const pool=new Pool({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
-})
-pool.on('connect',()=>{
-    console.log("Connected to the database");
-})
-pool.on("error",(err)=>{
-    console.error("Error connecting to the database",err);
-})
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+pool.on('connect', () => {
+  console.log("Connected to the database");
+});
+
 export default pool;
